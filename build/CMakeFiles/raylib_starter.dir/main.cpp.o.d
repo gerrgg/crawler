2313fb310bec12f5da18da5ec628dafa87bb2421
greg bastianelli <greg@gerrg.com>
2026-04-21 15:18:43 -0400

    Remove ignored files

--- build/CMakeFiles/raylib_starter.dir/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/raylib_starter.dir/main.cpp.o: /home/greg/cpp-final/main.cpp \
- /usr/include/stdc-predef.h \
- /home/greg/cpp-final/build/_deps/raylib-src/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
--- build/CMakeFiles/raylib_starter.dir/src/Player.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/raylib_starter.dir/src/Player.cpp.o: \
- /home/greg/cpp-final/src/Player.cpp /usr/include/stdc-predef.h \
- /home/greg/cpp-final/include/Player.h \
- /home/greg/cpp-final/build/_deps/raylib-src/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
